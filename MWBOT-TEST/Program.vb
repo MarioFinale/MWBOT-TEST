479101aa2c413d30358b518e2e0922b3bb7e825e
MarioFinale <mariofinale@hotmail.com>
2019-12-01 18:08:18 -0300

    +Files

---
+++ MWBOT-TEST/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+Imports MWBot.net.WikiBot
+
+Module Program
+    Sub Main(args As String())
+        Bot.StartUpCheck()
+        Console.WriteLine("Press any key to exit...")
+        Console.ReadKey()
+    End Sub
+End Module
